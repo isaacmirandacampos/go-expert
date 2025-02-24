9b0eba0ef8ced793bb8422d952e3965b59e125f7
Isaac de Miranda Campos <isaacdmcampos@Isaacs-MacBook-Air.local>
2025-02-24 13:40:28 -0300

    fix request timeout

--- 01-quotation-api/server/main.go
+++ 01-quotation-api/server/main.go
@@ -52,17 +52,22 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	}
 	json.NewEncoder(w).Encode(dollarConversionResponse)
 }
-
 func getDollarConversion(c *DollarConversion) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 200*time.Millisecond)
 	defer cancel()
+
 	req, err := http.NewRequestWithContext(ctx, "GET", "https://economia.awesomeapi.com.br/json/last/USD-BRL", nil)
 	if err != nil {
 		return errors.New("error creating request")
 	}
-	resp, err := http.DefaultClient.Do(req)
 
+	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
+		// Verifica se o erro foi causado pelo timeout
+		if ctx.Err() == context.DeadlineExceeded {
+			log.Println("Request Timeout economia.awesomeapi.com.br")
+			return errors.New("Request Timeout economia.awesomeapi.com.br")
+		}
 		return errors.New("error executing request")
 	}
 	defer resp.Body.Close()
@@ -76,13 +81,8 @@ func getDollarConversion(c *DollarConversion) error {
 	if err != nil {
 		return errors.New("error unmarshalling response")
 	}
-	select {
-	case <-ctx.Done():
-		log.Println("Request Timeout economia.awesomeapi.com.br")
-		return errors.New("Request Timeout economia.awesomeapi.com.br")
-	case <-time.After(200 * time.Millisecond):
-		return nil
-	}
+
+	return nil
 }
 
 func saveDollarConversion(c *DollarConversion) error {
@@ -90,35 +90,58 @@ func saveDollarConversion(c *DollarConversion) error {
 	if err != nil {
 		return err
 	}
+	defer db.Close()
+
+	// Usando contexto com timeout de 10ms para a transação
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Millisecond)
 	defer cancel()
-	defer db.Close()
+
+	// Inicia a transação
 	tx, err := db.BeginTx(ctx, nil)
 	if err != nil {
+		// Verifica se o erro foi causado pelo timeout
+		if ctx.Err() == context.DeadlineExceeded {
+			log.Println("Transaction Timeout")
+			return errors.New("Transaction Timeout")
+		}
 		return errors.New("error beginning transaction")
 	}
-	stmt, err := db.PrepareContext(ctx, `INSERT INTO usd_to_brl_conversions (
-		price) VALUES (?)`)
+
+	// Prepara a consulta de inserção
+	stmt, err := db.PrepareContext(ctx, `INSERT INTO usd_to_brl_conversions (price) VALUES (?)`)
 	if err != nil {
+		// Verifica se o erro foi causado pelo timeout
+		if ctx.Err() == context.DeadlineExceeded {
+			log.Println("Prepare Timeout")
+			return errors.New("Prepare Timeout")
+		}
 		return errors.New("error preparing statement")
 	}
 	defer stmt.Close()
-	_, err = stmt.Exec(
-		c.USDBRL.Price,
-	)
+
+	// Executa a inserção
+	_, err = stmt.Exec(c.USDBRL.Price)
 	if err != nil {
+		// Verifica se o erro foi causado pelo timeout
+		if ctx.Err() == context.DeadlineExceeded {
+			log.Println("Execute Timeout")
+			return errors.New("Execute Timeout")
+		}
 		return errors.New("error inserting into database")
 	}
+
+	// Commit da transação
 	if err = tx.Commit(); err != nil {
+		// Verifica se o erro foi causado pelo timeout
+		if ctx.Err() == context.DeadlineExceeded {
+			log.Println("Commit Timeout")
+			return errors.New("Commit Timeout")
+		}
 		return errors.New("error committing transaction")
 	}
-	select {
-	case <-ctx.Done():
-		log.Println("Insert Timeout")
-		return errors.New("Insert Timeout")
-	case <-time.After(10 * time.Millisecond):
-		return nil
-	}
+
+	// Nenhum erro ocorrido, a transação foi bem-sucedida
+	return nil
 }
 
 func db_connection() (*sql.DB, error) {
